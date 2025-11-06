b0e26f174506fe7cf340fe109aef65f8bf258d5a
manojmouryaneeli <mouryamanoj2505@gmail.com>
2025-11-06 22:23:10 +0530

    Initial PulseFeed Spring Boot project

---
+++ src/main/java/com/pulsefeed/backend/PulsefeedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pulsefeed.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulsefeedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulsefeedBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pulsefeed/backend/PulsefeedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulsefeed.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulsefeedBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
