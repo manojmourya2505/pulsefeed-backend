577368bbc1aa125462a23facc91d3797c2fe44a0
Manoj Mourya Neeli <mouryamanoj2505@gmail.com>
2025-11-08 17:06:33 +0530

    JWT Filter + Secure Routes

--- src/main/java/com/pulsefeed/backend/model/User.java
+++ src/main/java/com/pulsefeed/backend/model/User.java
@@ -3,6 +3,9 @@ package com.pulsefeed.backend.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "users")
 @Data
@@ -23,4 +26,49 @@ public class User {
 
     @Column(nullable = false)
     private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Post> post = new ArrayList<>();
+
+    public User(String username,String email, List<Post> post){
+        this.username = username;
+        this.email = email;
+        this.post = post;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getUserName(){
+        return username;
+    }
+
+    public void setUserName(String username){
+        this.username = username;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(){
+        this.email = email;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public List<Post> getPost(){
+        return post;
+    }
+
+    public void setPost(List<Post> post){
+        this.post = post;
+    }
 }
